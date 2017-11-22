064b750258c0f0cdff67d3deb73f4373c4bd5474
Catherine Cordova <ccordova41@gmail.com>
2017-11-22 05:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const token = '0adeed0af49a7b42e1cde6a67f91f8d5be5340f7';
+ 
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
+
 function getIssues() {
 }
 
